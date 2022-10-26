739ed273137ecec4a089fc797f178595477e9f60
seaarts <seaarts@gmail.com>
2022-10-26 17:58:20 -0400

    Add prop models, spatial domains, plan models

---
+++ src/loraplan/planning.py
@@ -0,0 +1,172 @@
+class Demands:
+    """ Demand point class.
+    """
+    
+    def __init__(self, locs, reqs):
+        """ Initialize class.
+        ::param locs:: location coordinates of points (numpy.array)
+        ::param reqs:: requirement vector (numpy.array)
+        """
+
+        # verify locations
+        locs = np.atleast_2d(locs)
+        
+        if reqs.ndim != 1:
+            raise ValueError("Requirements should be a 1-dimensional array.")
+        
+        if locs.shape[0] != len(reqs):
+            raise ValueError("Number of rows in locs and reqs do not match")
+            
+        elif locs.shape[1] != 2:
+            raise ValueError("Number of columns in locs not equal to 2.")
+        
+        self.locs = locs
+        self.reqs = reqs
+        
+    @property
+    def n_pts(self):
+        """The number of demand points.
+        """
+        return self.reqs.shape[0]
+
+
+
+
+class Facilities:
+    """ Facility class.
+    """
+    
+    def __init__(self, locs, cost):
+        """ Initialize class.
+        ::param locs:: location coordinates of points (numpy.array)
+        ::param cost:: requirement vector (numpy.array)
+        """
+
+        # verify locations
+        locs = np.atleast_2d(locs)
+        
+        if cost.ndim != 1:
+            raise ValueError("Cost should be a 1-dimensional array.")
+        
+        if locs.shape[0] != len(cost):
+            raise ValueError("Number of rows in locs and reqs do not match")
+            
+        elif locs.shape[1] != 2:
+            raise ValueError("Number of columns in locs not equal to 2.")
+        
+        self.locs = locs
+        self.cost = cost
+        
+    @property
+    def n_pts(self):
+        """Get number of facility locations.
+        """
+        return self.cost.shape[0]
+
+
+### Connection Quality models
+
+class ConnectionQuality(ABC):
+    """
+    Abstact base class for connection quality.
+    
+    This is a class of functions that take demand and facility location
+    arrays as inputs and return an (n_dems, n_facs) array of qualities.
+    
+    Quality models may be stochastic or deterministic. All stochastic
+    models should have a 'seed' variable that can take an RNG.
+    
+    Quality models may use the path loss models below. These take
+    distances in meters (m) unless stated otherwise.
+    """
+    
+    def __init__(self, name):
+        """Initialize Quality model.
+        ::param name:: str, a name for the quality model.
+        """
+        self.name = name
+        
+        
+    @abstractmethod
+    def __call__(self, locs_dem, locs_fac, *args, **kwargs):
+        """Abstact method for computing connection quality.
+        """
+        pass
+    
+    @property
+    @abstractmethod
+    def deterministic(self):
+        """Bool, True if no randomness is used.
+        """
+        pass
+    
+    
+
+    
+class LogErrorRate(ConnectionQuality):
+    """
+    Negatie log error rate quality model.
+    
+    This class of quality model combines path loss and iid gaussian noice:
+        1. demands have fixed transmitted power (p_tx)
+        2. a path loss model determines path loss (PL)
+        3. iid (0, sigma) gaussian noise sampled
+        4. received power (p_rx) follows from the link-budget:
+            
+            p_rx = p_tx - PL + noise
+            
+        5. a transmission is received iff minimum power (p_min) exceeded
+        
+        The probabiliy of success is expressed analytically as
+    
+            P[fail] = P[p_tw - PL + noise <= p_min ]
+                    = P[noise <= p_min - p_tx + PL ]
+                    = normal.cdf(p_min - p_tw + PL)
+    """
+    
+    def __init__(self, path_loss, p_tx, p_min, stdv):
+        """
+        Initialize LogErrorRate Quality model
+        ::param path_loss:: instance of LogLinearPathLoss
+        ::param p_tx:: scalar, transmitted power (dBm)
+        ::param p_min:: scalar, minimum threshold power (dBm)
+        ::pram stdv:: scalar >0, standard deviation of gaussian noise
+        """
+        
+        if not issubclass(type(path_loss), LogLinearPathLoss):
+            raise ValueError("'path_loss' must be subclass of 'LogLinearPathLoss'.")
+            
+        p_tx = check_scalar(p_tx, varname="p_tx")
+        p_min = check_scalar(p_min, varname="p_min")
+        stdv = check_scalar(stdv, varname="stdv")
+        
+        if stdv < 0:
+            raise ValueError("'stdv' must be non-negative.")
+            
+        self.path_loss = path_loss
+        self.p_tx = p_tx
+        self.p_min = p_min
+        self.stdv = stdv
+        
+        super().__init__(name='LogErrorRate')
+    
+    
+    def __call__(self, locs_dem, locs_fac):
+        """Compute negative log error rate.
+        """
+        
+        locs_dem = np.atleast_2d(locs_dem)
+        locs_fac = np.atleast_2d(locs_fac)
+        
+        distances = euclidean_distances(locs_dem, locs_fac)
+        path_loss = self.path_loss(distances)
+        
+        error_rate = norm.cdf(self.p_min + path_loss - self.p_tx, scale=self.stdv)
+        
+        return -np.log(error_rate)
+
+    @property
+    def deterministic(self):
+        """Boolean - always true. This method is deterministic.
+        """
+        return True
--- src/loraplan/simulation.py
+++ src/loraplan/simulation.py
@@ -1,12 +1,224 @@
 import numpy as np
 from abc import ABC, abstractmethod
+import collections.abc
+import warnings
+
+from scipy.stats import norm
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.metrics.pairwise import euclidean_distances
+
+
+
 
 #=========================================================
-#        Simulation tools and utilities
+#        General utilities and tools
 #=========================================================
 
 
-class TimeWindow():
+def _check_bbox(left, bottom, right, top):
+    """Check if given bbox coordinates are well-defined and non-empty.
+    """
+    
+    formatstr = "Coords should be (left, bot, right, top)."
+        
+    if left >= right:
+        raise ValueError(f"left <= right. {formatstr}")
+    if bottom >= top:
+        raise ValueError(f"top <= bottom. {formatstr}")
+    else:
+        return((left, bottom, right, top)) 
+
+
+def _check_scalar(x, varname="input"):
+    """
+    Check if x is a scalar.
+    
+    Notes
+    -----
+    This check is not robust against everything.
+    
+    """
+    if isinstance(x, (collections.abc.Sequence, dict, np.ndarray)):
+        raise ValueError(f"{varname} must be scalar.")
+    else:
+        return x
+
+
+
+
+#=========================================================
+#        (Spatial) Domains for Point Processes
+#=========================================================
+
+
+class SpatialDomain(ABC):
+    """
+    Spatial domain for a point process.
+    
+    Attributes
+    ----------
+    name : str, optional
+    
+    Notes
+    -----
+    Spatial domains must have an `area` 
+    """ 
+    
+    def __init__(self, name=None):
+        """Initialize a spatial domain.
+        
+        Parameters
+        ----------
+        name : str, optional
+        """
+        
+        self.name = name
+        
+    @property
+    @abstractmethod
+    def measure(self):
+        """A measure for the size of the domain.
+        
+        Notes
+        -----
+        For two-dimensional domains the area is a natural candidate.
+        For time-intervals the length is preferred.
+        """
+        pass
+    
+    @abstractmethod
+    def poisson(self, intensity, seed=None):
+        """
+        Sample homogeneous Poisson point process over Domain.
+        
+        Notes
+        -----
+        Sampling homogeneous Poisson Processes is a critical
+        part of sampling more involved methods. By requiring each
+        child class of SpatialDomain to support a `poisson` method
+        allows us to implement other porcesses on the SpatialDomain
+        level without repetition.
+        """
+        pass
+    
+    
+    def lgcp(self, intensity_latent, gaussian_process, seed=None):
+        """
+        Sample a log gaussian cox process.
+        
+        Parameters
+        ----------
+        latent_intensity : float
+            The intensity of a latent poisson process.
+        gaussian_process: sklearn.gaussian_process_gpr.GaussianProcessRegressor
+            A GP object from sklearn.
+        seed: optional
+    
+        Returns 
+        """
+        
+        rng = np.random.default_rng(seed)
+    
+        # verify GP is well-defined
+        if not isinstance(gaussian_process, GaussianProcessRegressor):
+            raise ValueError(f"The gaussian_process is not a {gp_type}")
+    
+        points_latent = self.poisson(intensity_latent, seed=rng)
+    
+        # form GP predictions at latent points
+        mean_int, std_int = gaussian_process.predict(points_latent, return_std=True)
+    
+        log_intensities = rng.normal(mean_int, std_int)
+        intensities = np.exp(log_intensities) / np.exp(log_intensities).max()
+    
+        # sample retained points given intensity
+        uniforms = rng.uniform(0,1, size=points_latent.shape[0])
+        ponits_observer = points_latent[uniforms <= intensities]
+    
+        return points_observed
+    
+
+
+class Rectangle(SpatialDomain):
+    """
+    A rectangular spatial domain for Point Process sampling.
+    """
+    
+    def __init__(self, bbox, name=None):
+        """Initialize a rectangle.
+        """
+        self.bbox = _check_bbox(bbox)
+        self.name = name
+        
+    @property
+    def width(self):
+        (left, bottom, right, top) = self.bbox
+        return right - left
+    
+    @property
+    def height(self):
+        (left, bottom, right, top) = self.bbox
+        return top - bottom
+    
+    @property
+    def area(self):
+        """Area of the rectangle.
+        """
+        return self.width * self.height
+    
+    @property
+    def measure(self):
+        """Get the area.
+        """
+        return self.area
+    
+    
+    def uniform(self, nPoints, seed=None):
+        """Sample points uniformly over the rectangle.
+        
+        Parameters
+        ----------
+        nPoints: int
+            Number of points to sample
+        seed: optional
+        """
+       
+        rng = np.random.default_rng(seed)
+        
+        nPoints = int(nPoints)
+        
+        if nPoints < 0:
+            raise ValueError("Number of points must be non-negative.")
+            
+        points = rng.uniform(0, 1, size=(n_pts, 2)) # standard uniforms
+        
+        points[:,0] *= self.width  # scale x-coordinates
+        points[:,1] *= self.height # scale y-coordinates
+        
+        return points
+    
+    
+    def poisson(self, intensity, seed=None):
+        """Sample a homogeneous Poisson Process over rectangle.
+        
+        Parameters
+        ----------
+        intensity: float
+            The intensity of the poisson process.
+        seed : optional
+        """
+        
+        rng = np.random.default_rng(seed)
+        
+        n_pts = rng.poisson(self.area * intensity)
+        
+        return self.uniform(n_pts, seed=seed)
+
+
+
+
+
+class TimeWindow(SpatialDomain):
     """A time interval.
     
     Attributes
@@ -54,11 +266,16 @@ class TimeWindow():
     
     @property
     def length(self):
-        """
-        Length of TimeWindow including buffers.
+        """Length of TimeWindow including buffers.
         """
         
         return self.tMax - self.tMin + 2*self.buff
+    
+    @property
+    def measure(self):
+        """Length of TimeWindow including buffers.
+        """
+        return self.length
     
     
     def contains(self, x):
@@ -102,9 +319,17 @@ class TimeWindow():
         low = self.tMin - self.buff
         high = self.tMax + self.buff
         
-        return rng.uniform(low=low, high=high, size=size)
+        return rng.uniform(low=low, high=high, size=size) 
     
+    def poisson(self, intensity, seed=None):
+        rng = np.random.default_rng(seed)
+        
+        nPoints = rng.poisson(self.measure * intensity)
+        
+        return self.uniform(nPoints, seed=seed)
     
+
+
 class ArrivalProcess(ABC):
     """Stochastic arrival process on TimeWindow"""
     
