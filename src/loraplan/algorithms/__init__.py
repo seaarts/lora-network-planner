0c6dd71691dd430bd6c120c030cac1f3a9bfd5bb
seaarts <seaarts@gmail.com>
2023-02-06 15:39:52 -0500

    Update GH acts and algo init [skip CI]

--- src/loraplan/algorithms/__init__.py
+++ src/loraplan/algorithms/__init__.py
@@ -4,5 +4,8 @@ Optimization algorithms for Network Planning (:mod:`loraplan.algorithms`)
 =========================================================================
 """
 
-from .greedy import *
-from .primalDual import *
+from .greedy import _greedy_update, greedy
+from .primalDual import _dual_update, primal_dual
+
+# pep8 compliance thing
+__all__ = ["greedy", "primal_dual"]
